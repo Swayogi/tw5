c232a8744805ef56ed722be200be0aab4d65660b
Swayogi <mrswayogi@gmail.com>
2025-06-17 01:02:34 +0530

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python  
-import sys  
-deptdic = {}  
-for line in sys.stdin:  
-    line = line.strip()  
-    dept,sal = line.split('\t')  
-    if dept in deptdic:  
-    deptdic[dept].append(int(sal))  
-    else:  
-    deptdic[dept] = []
-    deptdic[dept].append(int(sal))  
-for dept in deptdic.keys():  
-    sum_sal=sum(deptdic[dept])*1.0  
-    print '%s\t%s'%(dept,sum_sal)  
+#!/usr/bin/env python
+import sys
+deptdic = {}
+for line in sys.stdin:
+    line = line.strip()
+    dept, sal = line.split('\t')
+    if dept in deptdic:
+        deptdic[dept].append(int(sal))
+    else:
+        deptdic[dept] = []
+        deptdic[dept].append(int(sal))
+for dept in deptdic.keys():
+    sum_sal = sum(deptdic[dept]) * 1.0
+    print('%s\t%s' % (dept, sum_sal))
