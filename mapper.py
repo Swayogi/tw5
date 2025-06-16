a8fad88a3a323a5e9d1c2edcda8f90dae7ab2532
Swayogi <mrswayogi@gmail.com>
2025-06-17 00:50:47 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python  
 import sys   
 for line in sys.stdin:  
-    line = line.strip().split(",")  
-    if len(line) >= 2:  
-        dept = line[2]  
-        sal = line[3]  
-        print '%s\t%s' % (dept, sal)
+    line = line.strip()  
+    line = line.split(",")  
+    if len(line) >=2:  
+       dept = line[2]  
+       sal = line[3]  
+    print '%s\t%s' % (dept, sal)  
